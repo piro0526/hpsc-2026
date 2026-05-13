fb17eb90e9418816318289435cf1682378da80fb
piro0526 <uchiyama.h.b139@m.isct.ac.jp>
2026-05-14 05:00:56 +0900

    5/7

--- 04_simd/11_nbody.cpp
+++ 04_simd/11_nbody.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+#include <x86intrin.h>
 
 int main() {
   const int N = 16;
@@ -11,16 +12,33 @@ int main() {
     m[i] = drand48();
     fx[i] = fy[i] = 0;
   }
+  __m512 xvec = _mm512_loadu_ps(x);
+  __m512 yvec = _mm512_loadu_ps(y);
+  __m512 mvec = _mm512_loadu_ps(m);
+
+  __m512i jvec = _mm512_set_epi32(15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0);
   for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      if(i != j) {
-        float rx = x[i] - x[j];
-        float ry = y[i] - y[j];
-        float r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
-      }
-    }
+    __m512 xi = _mm512_set1_ps(x[i]);
+    __m512 yi = _mm512_set1_ps(y[i]);
+
+    __m512 rx = _mm512_sub_ps(xi, xvec);
+    __m512 ry = _mm512_sub_ps(yi, yvec);
+
+    __m512 r2 = _mm512_add_ps(_mm512_mul_ps(rx, rx),
+                              _mm512_mul_ps(ry, ry));
+
+    __m512 rinv  = _mm512_rsqrt14_ps(r2);
+    __m512 rinv3 = _mm512_mul_ps(_mm512_mul_ps(rinv, rinv), rinv);
+
+    __m512i ivec = _mm512_set1_epi32(i);
+    __mmask16 mask = _mm512_cmpneq_epi32_mask(ivec, jvec);
+
+    __m512 fxvec = _mm512_mul_ps(_mm512_mul_ps(rx, mvec), rinv3);
+    __m512 fyvec = _mm512_mul_ps(_mm512_mul_ps(ry, mvec), rinv3);
+
+    fx[i] -= _mm512_mask_reduce_add_ps(mask, fxvec);
+    fy[i] -= _mm512_mask_reduce_add_ps(mask, fyvec);
+    
     printf("%d %g %g\n",i,fx[i],fy[i]);
   }
 }
