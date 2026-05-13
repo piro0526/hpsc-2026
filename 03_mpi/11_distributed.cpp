7c819828e256eea9c3002f175300ff427fa72dd3
piro0526 <uchiyama.h.b139@m.isct.ac.jp>
2026-05-14 05:00:34 +0900

    4/30

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -13,24 +13,27 @@ int main(int argc, char** argv) {
   int size, rank;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  Body ibody[N/size], jbody[N/size];
+  int n = N / size;
+  Body ibody[n], jbody[n];
   srand48(rank);
-  for(int i=0; i<N/size; i++) {
+  for(int i=0; i<n; i++) {
     ibody[i].x = jbody[i].x = drand48();
     ibody[i].y = jbody[i].y = drand48();
     ibody[i].m = jbody[i].m = drand48();
     ibody[i].fx = jbody[i].fx = ibody[i].fy = jbody[i].fy = 0;
   }
-  int recv_from = (rank + 1) % size;
   int send_to = (rank - 1 + size) % size;
   MPI_Datatype MPI_BODY;
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
+  MPI_Win win;
+  MPI_Win_create(jbody, n * sizeof(Body), sizeof(Body), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
   for(int irank=0; irank<size; irank++) {
-    MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
-    MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    for(int i=0; i<N/size; i++) {
-      for(int j=0; j<N/size; j++) {
+    MPI_Win_fence(0, win);
+    MPI_Put(jbody, n, MPI_BODY, send_to, 0, n, MPI_BODY, win);
+    MPI_Win_fence(0, win);
+    for(int i=0; i<n; i++) {
+      for(int j=0; j<n; j++) {
         double rx = ibody[i].x - jbody[j].x;
         double ry = ibody[i].y - jbody[j].y;
         double r = std::sqrt(rx * rx + ry * ry);
@@ -41,11 +44,12 @@ int main(int argc, char** argv) {
       }
     }
   }
+  MPI_Win_free(&win);
   for(int irank=0; irank<size; irank++) {
     MPI_Barrier(MPI_COMM_WORLD);
     if(irank==rank) {
-      for(int i=0; i<N/size; i++) {
-        printf("%d %g %g\n",i+rank*N/size,ibody[i].fx,ibody[i].fy);
+      for(int i=0; i<n; i++) {
+        printf("%d %g %g\n",i+rank*n,ibody[i].fx,ibody[i].fy);
       }
     }
   }
